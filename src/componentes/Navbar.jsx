82609d94e5492843a39a115d5206d50af310d5f9
Cristhian Cantero <cristhian.canter@gmail.com>
2026-09-16 20:49:26 -0300

    Agregando clase 9

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -92,6 +92,11 @@ function Navbar() {
                     Clase 8 - useState y eventos
                   </Link>
                 </li>
+                <li>
+                  <Link to="/clase-9" className="dropdown-item">
+                    Clase 9 - Formularios y validaciones
+                  </Link>
+                </li>
               </ul>
             </li>
 
